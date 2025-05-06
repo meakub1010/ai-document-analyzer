6fac1a17a99e2d57f0f61d4a5d2ccbe2da7a6224
Muhammad <meakub86@gmail.com>
2025-05-06 00:33:11 -0400

    added ocr service rout

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.api import upload
 from app.core.logger import logger
+from app.api.v1.endpoints import ocr
 
 app = FastAPI()
 app.include_router(upload.router)
+app.include_router(ocr.router, prefix="/api/v1", tags=["OCR"])
 
 @app.get("/")
 def read_root():
