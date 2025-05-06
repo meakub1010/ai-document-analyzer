38a4b02e029979f94f0b1201e44e2bad00b6ae16
Muhammad <meakub86@gmail.com>
2025-05-06 00:37:03 -0400

    logging

--- app/api/v1/endpoints/ocr.py
+++ app/api/v1/endpoints/ocr.py
@@ -11,11 +11,12 @@ router = APIRouter()
 async def perform_ocr(file: UploadFile = File(...)):
     file_id = str(uuid.uuid4)
     file_path = f"/temp/{file_id}_{file.filename}"
+    logger.info("ocr end point hit")
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
     text = extract_text_from_pdf(file_path)
-
+    logger.info("pdf to text conversion done successfully")
     os.remove(file_path)
 
     return {"text": text}
